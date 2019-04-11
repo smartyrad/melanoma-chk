a994c385a9a19c3bdf3c4f5e5b1d6fc1c320e6f0
Reuben Varghese <reubenvarghese1@gmail.com>
2019-04-12 04:09:06 +0530

    Test

--- pricecheck/views.py
+++ pricecheck/views.py
@@ -15,43 +15,43 @@ from django.core.files.storage import FileSystemStorage
 
 #
 
-# import tensorflow as tf
-# import keras.backend.tensorflow_backend
+import tensorflow as tf
+import keras.backend.tensorflow_backend
 import os
-# os.environ["CUDA_VISIBLE_DEVICES"]="2"
-#
-# from keras.layers import Input, Dense
-# from keras.models import Model
-# from pricecheck.data import main, generate_data
-# from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
-# from keras.models import model_from_json
-# from keras.preprocessing import image
-# from keras.preprocessing.image import ImageDataGenerator
-#
-# import matplotlib.pyplot as plt
-# import pandas as pd
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from pricecheck.data import main, generate_data
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
+from keras.models import model_from_json
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+
+from sklearn import preprocessing
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+import itertools
+
+import random
+
+df = pd.read_csv(DATA_CONFIG['data_folder'] + 'csv/ISIC-2017_Training_Part3_GroundTruth.csv')
+class_label=df['melanoma']
+class_id=df['image_id']
+
+data = []
 #
-# from sklearn import preprocessing
-# from sklearn.metrics import roc_curve, auc, confusion_matrix
-# import itertools
-#
-# import random
-#
-# df = pd.read_csv(DATA_CONFIG['data_folder'] + 'csv/ISIC-2017_Training_Part3_GroundTruth.csv')
-# class_label=df['melanoma']
-# class_id=df['image_id']
-#
-# data = []
-# #
-# # #Load model
-# json_file = open(DATA_CONFIG['data_folder'] + 'weights/classification/nasnetsegmentedfullweights/classification.json', 'r')
-# model_json = json_file.read()
-# json_file.close()
-# load_model = model_from_json(model_json)
-# #Load weights into new model
-# load_model.load_weights(DATA_CONFIG['data_folder'] + "weights/classification/nasnetsegmentedfullweights/classification.h5")
-# print("Loaded model from disk")
+# #Load model
+json_file = open(DATA_CONFIG['data_folder'] + 'weights/classification/nasnetsegmentedfullweights/classification.json', 'r')
+model_json = json_file.read()
+json_file.close()
+load_model = model_from_json(model_json)
+#Load weights into new model
+load_model.load_weights(DATA_CONFIG['data_folder'] + "weights/classification/nasnetsegmentedfullweights/classification.h5")
+print("Loaded model from disk")
 
 # Create your views here.
 
@@ -83,10 +83,10 @@ def results(request):
     k = cv2.cvtColor(k, cv2.COLOR_BGR2RGB)
     k = cv2.resize(k, (331, 331))
     k = np.expand_dims(k, axis=0)
-    # y_pred = load_model.predict(k)
-    # classes = {'nevus': 0, 'melanoma': 1}
-    # thre = 0.5
+    y_pred = load_model.predict(k)
+    classes = {'nevus': 0, 'melanoma': 1}
+    thre = 0.5
     # # obtain class predictions from probabilities
-    # y_predi = (y_pred_c >= thre) * 1
+    y_predi = (y_pred_c >= thre) * 1
     y_predi = 1
     return render(request, 'pricecheck/results.html',{'predi': y_predi})
